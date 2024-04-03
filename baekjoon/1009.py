3c1b0ed802900406988d2eebdde51db2d92d99f9
moveonha <bycksan1104@naver.com>
2024-04-03 14:01:55 +0900

    good

--- baekjoon/1009.py
+++ baekjoon/1009.py
@@ -1,14 +1,16 @@
 T = int(input())
 
-result =[]
+result = []
 
 for _ in range(T):
     a, b = map(int, input().split())
-    num = (a**b)%10
-    if num == 0:
-        result.append(10)
+    a %= 10  # a를 일의 자리로 만듦
+    if a == 0:
+        result.append(10)  # 0^b의 경우 마지막 자리는 10이다.
     else:
-        result.append(num)
-        
+        b %= 4  # 주기성에 따라 b를 4로 나눈 나머지 값만 필요하다.
+        if b == 0: b = 4  # b가 4의 배수일 경우 주기성에 따라 나머지가 0이 되므로, 4로 바꿔준다.
+        result.append((a**b) % 10)
+
 for num in result:
     print(num)
