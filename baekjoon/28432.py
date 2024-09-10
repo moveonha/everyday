8c5df57562d1a6a26e5ee3eed4eb90fb257d6b7c
moveonha <bycksan1104@naver.com>
2024-09-10 14:36:48 +0900

    20240910

--- baekjoon/28432.py
+++ baekjoon/28432.py
@@ -1,21 +1,23 @@
-N = int(input())  # 끝말잇기 기록의 개수
-records = [input() for _ in range(N)]  # 끝말잇기 기록들을 리스트에 저장
-
-M = int(input())  # 후보 단어의 개수
-candidates = [input() for _ in range(M)]  # 후보 단어들을 리스트에 저장
-
-# 가능한 후보들을 찾아서 저장
-possible_candidates = []
-
-for candidate in candidates:
-    for record in records:
-        if record == "?":
-            continue
-        if record[-1] == candidate[0]:
-            possible_candidates.append(candidate)
-            break
-
-# 가능한 후보 중 첫 번째 후보를 출력
-print(possible_candidates[0])
+N = int(input())
+words =[]
+candidates = []
 
 
+for _ in range(N):
+    word = str(input())
+    words.append(word)
+
+for i in range(len(words)):
+    if words[i] == "?":
+        print(i+1)
+        answer = []
+        answer.append(words[i-1])
+        answer.append(words[i+1])
+
+for i in range(i-1):
+    candidate = str(input())
+    candidates.append(candidate)
+    
+for i in range(N):
+    if words[i] == candidates[i]:
+        continue
